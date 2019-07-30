6a5923b4f767155d8f21ae8f22e464d4f2a24a3d
vikram <vikrambser@gmail.com>
2019-07-30 15:08:02 +0530

    Hello

--- hello.java
+++ hello.java
@@ -3,5 +3,6 @@ import java.util.*;
 public class Hello{
 	public static void main(String[] args) {
 		for(int i=0;i<6;i++){
-		System.out.println("Hello"); } } 
+		System.out.println("Hello"); }
+		 } 
 	}
